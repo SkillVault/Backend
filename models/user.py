c8af851d3434dafa3b5632fd382321791ff664ac
BIBS23 <bibinoottukulathil@gmail.com>
2024-04-16 13:45:00 +0530

    code updated3

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from typing import List,Optional
 
 
 
-
 class CreateUser(BaseModel):
    
     mailid: str
@@ -36,19 +35,20 @@ class CandidateSignup(BaseModel):
     about_me: Optional[str] = None
     skills: Optional[str] = None
     interview_scores: Optional[str] = None
-    
-class CandidateLogin(BaseModel):
-    email: str
-    password: str
-     
-    
-class Candidate(BaseModel):
-    username: str
-    email: str
-    password: str
+
+
+
+class Address(BaseModel):
+    first_line: str
+    country: str
+    state: str
+    city: str
+    pincode: int
+
+class BaseUser(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
-    address: Optional[str] = None
+    address: Optional[Address] = None
     job_role: Optional[str] = None
     company: Optional[str] = None
     experience: Optional[int] = None
@@ -56,46 +56,80 @@ class Candidate(BaseModel):
     photo: Optional[str] = None
     about_me: Optional[str] = None
     skills: Optional[str] = None
-    interview_scores: Optional[str] = None 
+    interview_scores: Optional[str] = None
+
+
+class UpdateUser(BaseModel):
+    username: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    address: Optional[Address] = None
+    phone_number: Optional[str] = None
+    job_role: Optional[str] = None
+    company: Optional[str] = None
+    experience: Optional[int] = None
+    resume: Optional[str] = None
+    photo: Optional[str] = None
+    about_me: Optional[str] = None
+
+class Candidate(BaseUser):
+    username: str
+    email: str
+    password: str
+
+    
+class Login(BaseModel):
+    email: str
+    password: str
 
 
 class GoogleUser(BaseModel):
-    user_name: str
-    user_mail: str
+    username: str
+    email: str
     profile_url: str
+    experience: int
+    skills:str
+    resume:str
     first_name: str
     last_name: str
     country: str
     state: str
     city: str
     postal_code: str
-    about: str
+    about_me: str
     address: str
 
 
 class GetUser(BaseModel):
-    user_name: str
-    user_mail: str
-    profile_url: str
+    username: str
+    email: str
+    skills:str
+    resume:str
+    photo: str
     first_name: str
     last_name: str
+    experience: int
     country: str
     state: str
     city: str
     postal_code: str
-    about: str
+    about_me: str
     address: str
 
 class UpdateGoogleUser(BaseModel):
     
-    user_name : str
+    username : str
+    experience: int
+    resume:str
+    skills:str
+    photo:str
     first_name: str
     last_name: str
     country: str
     state: str
     city: str
     postal_code: str
-    about: str
+    about_me: str
     address: str
 
 
