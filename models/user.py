372b5791774fb72d1b87247ec7d403519800b77e
BIBS23 <bibinoottukulathil@gmail.com>
2024-04-18 16:25:56 +0530

    fixing2

--- models/user.py
+++ models/user.py
@@ -33,7 +33,7 @@ class UpdateUser(BaseModel):
     company: Optional[str] = None
     experience: Optional[int] = None
     resume: Optional[str] = None
-    photo:  [UploadFile] = File(...)
+    photo: Optional[str] = None
     about_me: Optional[str] = None
 
 class Candidate(BaseUser):
